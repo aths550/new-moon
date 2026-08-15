25529d873ac7559229ab3c6eb159d4994b37eb0b
Atharva Sandip Narute <atharvanarute29@gmail.com>
2026-08-16 04:09:24 +0530

    fix(lint): fix unbound method for computeCommitment export

--- api/src/auction-api.ts
+++ api/src/auction-api.ts
@@ -19,7 +19,8 @@ import { deployContract, findDeployedContract } from '@midnight-ntwrk/midnight-j
 import { map, tap, type Observable } from 'rxjs';
 import { toHex } from '@midnight-ntwrk/midnight-js-utils';
 
-export const computeCommitment = Auction.pureCircuits.computeCommitment;
+export const computeCommitment = (salt: Uint8Array, amount: bigint) =>
+  Auction.pureCircuits.computeCommitment(salt, amount);
 
 export interface DeployedAuctionAPI {
   readonly deployedContractAddress: ContractAddress;
